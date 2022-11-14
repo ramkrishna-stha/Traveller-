1d161b631175ef941d8ea8e7321199c8476bbed1
ramkrishna-stha <sthram132@gmail.com>
2022-11-14 21:36:03 +0545

    project setup

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {  
+
+  $('.slider-testimonals').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+  slidesToScroll: 1,
+      adaptiveHeight: true,
+      arrows: true
+    });
+  })
